45b38134f722028bad1306039977c4d5f3276af8
dionCavanaugh <dioncavanaugh@hotmail.com>
2017-10-11 03:23:08 -0400

    added necessary code for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
- 
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "watson_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    //local host
+      connection = mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "root",
+          database: "watson_db",
+      });
+  };
 // Make connection.
 connection.connect(function(err) {
   if (err) {
